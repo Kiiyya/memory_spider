dd775768697313f3ba9c0c79c41742fd785e3f6a
Kiiya <snoewflaek@gmail.com>
2021-01-28 17:44:44 +0100

    x

--- src/lib.rs
+++ src/lib.rs
@@ -35,50 +35,19 @@ struct ProcessHandle<A: Arch> {
 }
 
 /////////////////////////
-// enum Assert<const COND: bool> {}
-// trait IsTrue {}
-// impl IsTrue for Assert<true> {}
 
-trait TypeGen<T, const N: usize> {
-    type Result;
+impl <A: Arch> ProcessHandle<A> {
+    // pub fn point<T, const N: usize>(&self, offsets: [A::Pointer; N]) -> () {
+    //     if N > 0 {
+    //         self.point(offsets[1..].try_into().unwrap());
+    //     }
+    //     todo!();
+    // }
+
+    pub fn point_somewhere(&self, offset: A::Pointer) -> Remote<&()> {
+        
+    }
 }
-
-struct Wrap<T>(PhantomData<T>);
-struct Helper;
-
-impl <T> TypeGen<T, 0> for Helper { type Result = T; }
-impl <T> TypeGen<T, 1> for Helper { type Result = Wrap<T>; }
-impl <T> TypeGen<T, 2> for Helper { type Result = Wrap<Wrap<T>>; }
-impl <T> TypeGen<T, 3> for Helper { type Result = Wrap<Wrap<Wrap<T>>>; }
-impl <T> TypeGen<T, 4> for Helper { type Result = Wrap<Wrap<Wrap<Wrap<T>>>>; }
-impl <T> TypeGen<T, 5> for Helper { type Result = Wrap<Wrap<Wrap<Wrap<Wrap<T>>>>>; }
-
-
-// impl <T, const N: usize> TypeGen<T, N> for Helper
-//     where
-//         Assert::<{N > 0}>: IsTrue,
-//         [(); N - 1]:,
-// {
-//     type Result = Wrap<<Helper as TypeGen<T, {N - 1}>>::Result>;
-// }
-
-// produce `Wrap<Wrap<Wrap<...>>>` with exactly N `Wrap`s.
-pub fn wrap<T, const N: usize>(array: [T; N]) -> <Helper as TypeGen<T, N>>::Result {
-    todo!()
-}
-
-// const fn matthh(n: usize) -> usize {
-//     n - 1
-// }
-
-// impl <A: Arch> ProcessHandle<A> {
-//     pub fn point<T, const N: usize>(&self, offsets: [A::Pointer; N]) -> <Helper as TypeGen<N, false>>::Result {
-//         if N > 0 {
-//             self.point(offsets[1..].try_into().unwrap());
-//         }
-//         todo!();
-//     }
-// }
 
 fn user() -> Result<()> {
     // let ph : ProcessHandle<Arch64Little> = ProcessHandle { _phantom: PhantomData };
